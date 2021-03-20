95ac51fb3c0bc6a30fb1bd214abf47828b9c400e
damansandhu <73917329+damansandhu@users.noreply.github.com>
2021-03-19 20:35:52 -0700

    Add files via upload

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,4 +1,22 @@
+library(dplyr)    # alternatively, this also loads %>%
+
 MechaCar_mpg <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
-lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=MechaChar_mpg) # Generate multiple linear models
+
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=MechaCar_mpg) # Generate multiple linear models
+
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=MechaCar_mpg)) # Generate summary of multiple linear models
+
+Suspension_Coil <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+
+total_summary <- Suspension_Coil %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI)) # Create summary table with multiple columns
+
+lot_summary <- Suspension_Coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI)) # Create lot summary
+
+# Suspension Coil T-Test
+t.test(Suspension_Coil$PSI, mu=1500) #compare sample versus population means
+# Perform t-test on Lot 1
+t.test(subset(Suspension_Coil,Manufacturing_Lot=="Lot1")$PSI,mu = 1500)
+# Perform t-test on Lot 2
+t.test(subset(Suspension_Coil,Manufacturing_Lot=="Lot2")$PSI,mu = 1500)
+# Perform t-test on Lot 3
+t.test(subset(Suspension_Coil,Manufacturing_Lot=="Lot3")$PSI,mu = 1500)
